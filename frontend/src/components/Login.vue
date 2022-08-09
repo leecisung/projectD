355942afb0d7069419bc0be2d638adb4a38d47fd
leecisung <leecisung@gmail.com>
2022-08-09 20:06:38 +0900

    sigup.vue login.vue 운학님과 싱크 맞춤 chatroom.js 오타수정(25번째 줄)

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,16 +1,22 @@
 <template>
 <form @submit.prevent="submitForm">
-  <div class="hello">
+  <div  class="login-box">
     <div class="input_row">
       <label for="userid">아이디</label>
-      <input type="text" id="userid" v-model="userid" />
+      <input type="text" id="userid" placeholder="아이디를 입력해주세요" v-model="userid" />
     </div>
     <div class="input_row">
       <label for="password">비밀번호</label>
-      <input type="password" id="password" v-model="password" />
+      <input type="password" id="password" placeholder="패스워드를 입력해주세요"  v-model="password" />
     </div>
-    <button type="submit">로그인</button>
-    <a href="/signUp">가입하기</a>
+    <div class="btn-box">
+      <router-link to="/signup">
+        <span class="login-btn"><span>회원가입하기</span></span>
+      </router-link>
+      <!-- <router-link to="/"> -->
+        <button type="submit" class="login-btn"><span>로그인하기</span></button>
+      <!-- </router-link> -->
+    </div>
   </div>
   </form>
 </template>
@@ -53,7 +59,10 @@ export default {
 </script>
 
 <style scoped>
-input {
-  border: 1px solid #000;
-}
+.login-box{margin:0px auto;width:280px;}
+.login-box label{display:block;padding:15px 0 10px;font-size:15px;text-align:left;font-weight:600;}
+.login-box input{padding:10px;width:100%;font-size:15px;border-bottom:1px solid #888;box-sizing:border-box;}
+.login-box .btn-box{margin-top:20px;}
+.login-box .login-btn{margin:0 5px}
+.login-box .login-btn span{display:inline-block;margin-top:0px;width:100px;height:30px;color:#666;font-size:15px;line-height:30px;border:1px solid #888;border-radius:20px;cursor:pointer;}
 </style>
--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,13 +1,13 @@
 <template>
- <form @submit.prevent="submitForm">
-  <div class="app">
+<form @submit.prevent="submitForm">
+  <div class="login-box">
     <div class="input_row">
       <label for="userid">아이디</label>
-      <input type="text" id="userid" v-model="userid" />
+      <input type="text" id="userid" placeholder="사용하실 아이디를 입력해주세요" v-model="userid" />
     </div>
     <div class="input_row">
       <label for="name">이름</label>
-      <input type="text" id="name" v-model="name" />
+      <input type="text" id="name" placeholder="이름을 입력해주세요" v-model="name" />
     </div>
     <!-- <div class="input_row">
       <label for="address">주소</label>
@@ -15,11 +15,14 @@
     </div> -->
     <div class="input_row">
       <label for="password">비밀번호</label>
-      <input type="password" id="password" v-model="password" />
+      <input type="password" id="password" placeholder="사용하실 비밀번호를 입력해주세요" v-model="password" />
     </div>
-    <button type="submit">가입하기</button>
+    <div class="btn-box">
+      <!-- <router-link to="/"> -->
+        <button type="submit" class="login-btn"><span>가입하기</span></button>
+      <!-- </router-link> -->
+    </div>
   </div>
-   <p>{{ logMessage }}</p>
   </form>
 </template>
 <script>
@@ -47,8 +50,8 @@ export default {
         // address: this.address,
       };
       const { data } = await registerUser(userData);
+      this.$router.push('/login');
       
-      this.logMessage = `${data.name} 님이 가입되었습니다.`;
       
       // 가입 후 폼 초기화
       this.initForm();
@@ -64,7 +67,10 @@ export default {
 </script>
 
 <style scoped>
-input {
-  border: 1px solid #000;
-}
+.login-box{margin:0px auto;width:280px;}
+.login-box label{display:block;padding:15px 0 10px;font-size:15px;text-align:left;font-weight:600;}
+.login-box input{padding:10px;width:100%;font-size:15px;border-bottom:1px solid #888;box-sizing:border-box;}
+.login-box .btn-box{margin-top:20px;}
+.login-box .login-btn{margin:0 5px}
+.login-box .login-btn span{display:inline-block;margin-top:0px;width:100px;height:30px;color:#666;font-size:15px;line-height:30px;border:1px solid #888;border-radius:20px;cursor:pointer;}
 </style>
